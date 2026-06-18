dd32d18eba9e8ed17ccc04fa8918afac622fe660
Rohitha <rohitha@gmail.com>
2026-06-18 17:34:37 +0530

    Fix ProductCard filename

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-import ProductCard from '../components/ProductCard'
+import ProductCard from '../components/Productcard'
 
 const Products = ({ addToCart }) => {
 
